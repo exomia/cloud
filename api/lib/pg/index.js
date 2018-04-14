6031b841e887594c3def1e6a0aeeac397548fb31
Daniel Baetz <baetz.daniel@gmail.com>
2018-04-14 23:33:44 +0200

    bug fix and ref

--- api/lib/pg/index.js
+++ api/lib/pg/index.js
@@ -7,14 +7,14 @@ pool.on('error', (err, client) => {
     console.error('idle client error', err, client)
 })
 
-class LB {
+class _LB {
     constructor(qp, ...values) {
         this.qp = qp
         this.values = values
     }
 }
 
-class E {
+class _E {
     constructor(qp) {
         this.qp = qp
     }
@@ -24,42 +24,42 @@ class E {
  * late binds a given sub query
  * @param {*} qp
  * @param {*} values
- * @returns {LB} new LB
+ * @returns {_LB} new _LB
  */
 export const lb = (qp, ...values) => {
-    return new LB(qp, ...values)
+    return new _LB(qp, ...values)
 }
 
 /**
  * use the given argument explicit in the query
  * @param {*} qp
  * @param {*} values
- * @returns {E} new E
+ * @returns {_E} new _E
  */
 export const e = (qp, ...values) => {
     let res = ''
-    for (let i = 0; i < qp.length - 1; i++) {
+    for (let i = 0; i < qp.length - 1; ++i) {
         res += qp[i] + values[i].toString()
     }
-    return new E(res + qp[qp.length - 1])
+    return new _E(res + qp[qp.length - 1])
 }
 
 export const query = async (qp, ...values) => {
     let query = ''
     let index = 1
-    for (let i = 0; i < qp.length - 1; i++) {
-        if (values[i] && values[i] instanceof LB) {
+    for (let i = 0; i < qp.length - 1; ++i) {
+        if (values[i] && values[i] instanceof _LB) {
             query += qp[i]
-            for (let k = 0; k < values[i].qp.length - 1; k++) {
-                query += values[i].qp[k] + '$' + index++
+            for (let k = 0; k < values[i].qp.length - 1; ++k) {
+                query += `${values[i].qp[k]}$${index++}`
             }
             query += values[i].qp[values[i].qp.length - 1]
             values.splice(i, 1, ...values[i].values)
-        } else if (values[i] && values[i] instanceof E) {
+        } else if (values[i] && values[i] instanceof _E) {
             query += qp[i] + values[i].qp
             values.splice(i, 1)
         } else {
-            query += qp[i] + '$' + index++
+            query += `${qp[i]}$${index++}`
         }
     }
     query += qp[qp.length - 1]
@@ -78,16 +78,16 @@ export const query = async (qp, ...values) => {
 /**
  * join multiple late bindings to a single one separated with a comma
  * @param {*} args
- * @returns {LB} new LB
+ * @returns {_LB} new _LB
  */
 export const lbjoin = (...args) => {
     const qp = ['']
     const values = []
 
-    for (let i = 0; i < args.length - 1; i++) {
-        if (args[i] && args[i] instanceof LB) {
+    for (let i = 0; i < args.length - 1; ++i) {
+        if (args[i] && args[i] instanceof _LB) {
             qp[qp.length - 1] += args[i].qp[0]
-            for (let k = 1; k < args[i].qp.length - 1; k++) {
+            for (let k = 1; k < args[i].qp.length - 1; ++k) {
                 qp.push(args[i].qp[k])
             }
             qp.push(args[i].qp[args[i].qp.length - 1] + ', ')
@@ -97,13 +97,13 @@ export const lbjoin = (...args) => {
     if (
         args.length > 0 &&
         args[args.length - 1] &&
-        args[args.length - 1] instanceof M
+        args[args.length - 1] instanceof _LB
     ) {
         qp[qp.length - 1] += args[args.length - 1].qp[0]
-        for (let k = 1; k < args[args.length - 1].qp.length; k++) {
+        for (let k = 1; k < args[args.length - 1].qp.length; ++k) {
             qp.push(args[args.length - 1].qp[k])
         }
         values.push(...args[args.length - 1].values)
     }
-    return new LB(qp, values)
+    return new _LB(qp, values)
 }
