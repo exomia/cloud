8a50731a37a314ce53ecd067bd8be33454fc4bc1
Daniel Baetz <baetz.daniel@gmail.com>
2018-04-24 16:51:40 +0200

    changed ath system

--- api/lib/jwt/index.js
+++ api/lib/jwt/index.js
@@ -4,23 +4,19 @@ import { getUserPassword } from '../pg/user/auth'
 import { JE1002 } from '../error'
 
 export async function sign(res, { email, password, flags }, stayLoggedIn) {
-    res.setHeader('Access-Control-Expose-Headers', 'x-token, x-refresh-token')
+    res.setHeader('Access-Control-Expose-Headers', 'x-token, x-refresh-s-token, x-refresh-l-token')
     res.setHeader('x-token', jwt.sign({ email, flags }, config.SECRET_T + password, config.jwt_options_t))
     res.setHeader(
-        'x-refresh-token',
+        !stayLoggedIn ? 'x-refresh-s-token' : 'x-refresh-l-token',
         jwt.sign({ stayLoggedIn: !!stayLoggedIn }, password + config.SECRET_RT, config.jwt_options_rt)
     )
 }
 export async function jwt_init(req, res, next) {
-    const t =
-        req.headers['x-token'] ||
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIiwic3RheUxvZ2dlZEluIjpmYWxzZSwiaWF0IjoxNTI0NDMwMzczLCJleHAiOjE1MjQ0MzEyNzN9.NtXZ6G4YMnEhtMjXa8UwHfQamDbYi7lU9TgJvrEVAcg'
-
+    const t = req.headers['x-token']
     if (!t) {
         res.status(200)
         return res.json(JE1002)
     }
-
     let payload = jwt.decode(t)
     if (!payload || !payload.email) {
         res.status(200)
@@ -43,42 +39,26 @@ export async function jwt_init(req, res, next) {
         }
     }
 
-    const rt =
-        req.headers['x-refresh-token'] ||
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF5TG9nZ2VkSW4iOmZhbHNlLCJpYXQiOjE1MjQ0MzAzNzMsImV4cCI6MTUyNTAzNTE3M30._Tzj4Z4ewcPH3ALJBRMsq0_cXg9_J4vrDALTqfmq5E8'
-
+    const rt = req.headers['x-refresh-token']
     if (!rt) {
         res.status(200)
         return res.json(JE1002)
     }
 
     try {
-        jwt.verify(rt, password + config.SECRET_RT, config.jwt_verify_options)
-        req.jwt = payload
-        res.setHeader('Access-Control-Expose-Headers', 'x-token')
+        let payload_rt = jwt.verify(rt, password + config.SECRET_RT, config.jwt_verify_options)
+        res.setHeader('Access-Control-Expose-Headers', 'x-token, x-refresh-s-token, x-refresh-l-token')
         res.setHeader('x-token', jwt.sign({ email: payload.email }, config.SECRET_T + password, config.jwt_options_t))
 
+        res.setHeader(
+            !payload_rt.stayLoggedIn ? 'x-refresh-s-token' : 'x-refresh-l-token',
+            jwt.sign(payload_rt, password + config.SECRET_RT, config.jwt_options_rt)
+        )
+
+        req.jwt = payload
         return next()
     } catch (err) {
-        if (err.name !== 'TokenExpiredError') {
-            res.status(200)
-            return res.json(JE1002)
-        }
-    }
-
-    let payload_rt = jwt.decode(rt)
-    if (!payload_rt || !payload_rt.stayLoggedIn) {
         res.status(200)
         return res.json(JE1002)
     }
-
-    res.setHeader('Access-Control-Expose-Headers', 'x-token, x-refresh-token')
-    res.setHeader('x-token', jwt.sign({ email: payload.email }, config.SECRET_T + password, config.jwt_options_t))
-    res.setHeader(
-        'x-refresh-token',
-        jwt.sign({ stayLoggedIn: true }, password + config.SECRET_RT, config.jwt_options_rt)
-    )
-
-    req.jwt = payload
-    return next()
 }
