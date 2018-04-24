c035275109ea210f2e8564e898bec9948d1087dd
Daniel Baetz <baetz.daniel@gmail.com>
2018-04-24 20:14:19 +0200

    bug fix

--- api/routes/v1/directory/index.js
+++ api/routes/v1/directory/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { listAllDirectories, getDirectoryInfo, addDirectory, updateDirectory, deleteDirectory } from '../lib/pg_helper'
-import { E500, E400, JE500, JE12 } from '../lib/error'
+import { JE500, JE1001 } from '../lib/error'
 import { xor_encode, xor_decode } from '../lib/util'
 
 const router = express.Router()
@@ -10,12 +10,12 @@ router.get('/:directory_id?', async ({ jwt: { email }, params: { directory_id } 
 
     const directories = await listAllDirectories(email, directory_uuid)
     if (!directories) {
-        return next(E500)
+        return res.json(JE500)
     }
 
     const files = await listAllFiles(email, directory_uuid)
     if (!files) {
-        return next(E500)
+        return res.json(JE500)
     }
 
     const dires = await getDirectoryInfo(email, directory_uuid)
@@ -35,10 +35,10 @@ router.get('/:directory_id?', async ({ jwt: { email }, params: { directory_id } 
     })
 })
 
-router.post('/add', async ({ jwt: { payload }, body: { name, parent_directory_id } }, res, next) => {
+router.post('/add', async ({ jwt: { email }, body: { name, parent_directory_id } }, res, next) => {
     const parent_directory_uuid = xor_decode(parent_directory_id)
 
-    let result = await addDirectory(payload.email, name, 0, parent_directory_uuid)
+    let result = await addDirectory(email, name, parent_directory_uuid)
     if (!result) {
         return res.json(JE500)
     }
@@ -52,13 +52,13 @@ router.post('/add', async ({ jwt: { payload }, body: { name, parent_directory_id
     })
 })
 
-router.post('/rename', async ({ jwt: { payload }, body: { directory_id, new_name } }, res, next) => {
+router.post('/rename', async ({ jwt: { email }, body: { directory_id, new_name } }, res, next) => {
     const directory_uuid = xor_decode(directory_id)
     if (!directory_uuid || !new_name || new_name.length <= 0) {
-        return res.json(JE12)
+        return res.json(JE1001)
     }
 
-    let result = await updateDirectory(payload.email, directory_uuid, { new_name })
+    let result = await updateDirectory(email, directory_uuid, { new_name })
     if (!result) {
         return res.json(JE500)
     }
@@ -71,14 +71,14 @@ router.post('/rename', async ({ jwt: { payload }, body: { directory_id, new_name
     })
 })
 
-router.post('/move', async ({ jwt: { payload }, body: { directory_id, new_parent_directory_id } }, res, next) => {
+router.post('/move', async ({ jwt: { email }, body: { directory_id, new_parent_directory_id } }, res, next) => {
     const directory_uuid = xor_decode(directory_id)
     const new_parent_directory_uuid = xor_decode(new_parent_directory_id)
     if (!directory_uuid || !new_parent_directory_uuid) {
-        return res.json(JE12)
+        return res.json(JE1001)
     }
 
-    let result = await updateDirectory(payload.email, directory_uuid, { new_parent_directory_uuid })
+    let result = await updateDirectory(email, directory_uuid, { new_parent_directory_uuid })
     if (!result) {
         return res.json(JE500)
     }
@@ -90,13 +90,13 @@ router.post('/move', async ({ jwt: { payload }, body: { directory_id, new_parent
     })
 })
 
-router.post('/remove', async ({ jwt: { payload }, body: { directory_id } }, res, next) => {
+router.post('/remove', async ({ jwt: { email }, body: { directory_id } }, res, next) => {
     const directory_uuid = xor_decode(directory_id)
     if (!directory_id) {
-        return res.json(JE1002)
+        return res.json(JE1001)
     }
 
-    let result = await deleteDirectory(payload.email, directory_id)
+    let result = await deleteDirectory(email, directory_id)
     if (!result) {
         return res.json(JE500)
     }
