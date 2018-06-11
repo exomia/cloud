83141acfad84d5c3ed1e8c077d8d9d0daade13c0
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-06-11 14:10:58 +0200

    Removed eslint. Added server side for nuxt

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -78,17 +78,6 @@ module.exports = {
         /*
         ** You can extend webpack config here
         */
-        extend(config, ctx) {
-            // Run ESLint on save
-            if (ctx.isDev && ctx.isClient) {
-                config.module.rules.push({
-                    enforce: 'pre',
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    exclude: /(node_modules)/
-                })
-            }
-        }
     },
 
     /*
