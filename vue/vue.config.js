922402c23c531b8855e3ce2de2cc5105397eb8b6
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-08-30 22:20:51 +0200

    Image optimization test

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,7 +1,46 @@
 const path = require('path')
 
+const { ImageminWebpackPlugin } = require('imagemin-webpack')
+
+// Before importing imagemin plugin make sure you add it in `package.json` (`dependencies`) and install
+const imageminGifsicle = require('imagemin-gifsicle')
+const imageminJpegtran = require('imagemin-jpegtran')
+const imageminOptipng = require('imagemin-optipng')
+const imageminSvgo = require('imagemin-svgo')
+
 module.exports = {
     chainWebpack: config => {
+        config
+            .plugin('imagemin-webpack-plugin')
+            .use(ImageminWebpackPlugin)
+            .tap(() => {
+                return [
+                    {
+                        imageminOptions: {
+                            // Lossless optimization with custom option
+                            // Feel free to experement with options for better result for you
+                            plugins: [
+                                imageminGifsicle({
+                                    interlaced: true
+                                }),
+                                imageminJpegtran({
+                                    progressive: false
+                                }),
+                                imageminOptipng({
+                                    optimizationLevel: 5
+                                }),
+                                imageminSvgo({
+                                    removeViewBox: true
+                                })
+                            ],
+                            loader: false,
+                            include: './src'
+                        }
+                    }
+                ]
+            })
+
+        /* Inline svg */
         const svgRule = config.module.rule('svg')
         svgRule.uses.clear()
         svgRule.use('vue-svg-loader').loader('vue-svg-loader')
