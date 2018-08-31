aca73847d5f8e08b6a86c094b3cd4fb68ec03258
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-08-31 21:12:18 +0200

    Implemented image optimization & gzip/brotli compression

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,49 +1,110 @@
 const path = require('path')
-
+//
 const { ImageminWebpackPlugin } = require('imagemin-webpack')
-
-// Before importing imagemin plugin make sure you add it in `package.json` (`dependencies`) and install
 const imageminGifsicle = require('imagemin-gifsicle')
-const imageminJpegtran = require('imagemin-jpegtran')
+const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminOptipng = require('imagemin-optipng')
-const imageminSvgo = require('imagemin-svgo')
+//
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin')
 
 module.exports = {
     chainWebpack: config => {
-        config
-            .plugin('imagemin-webpack-plugin')
-            .use(ImageminWebpackPlugin)
-            .tap(() => {
-                return [
-                    {
-                        imageminOptions: {
-                            // Lossless optimization with custom option
-                            // Feel free to experement with options for better result for you
-                            plugins: [
-                                imageminGifsicle({
-                                    interlaced: true
-                                }),
-                                imageminJpegtran({
-                                    progressive: false
-                                }),
-                                imageminOptipng({
-                                    optimizationLevel: 5
-                                }),
-                                imageminSvgo({
-                                    removeViewBox: true
-                                })
-                            ],
-                            loader: false,
-                            include: './src'
-                        }
+        if (process.env.NODE_ENV === 'production') {
+            /* Image Compression */
+            const imageRule = config.module.rule('images')
+            imageRule.uses.clear()
+
+            config.module
+                .rule('images')
+                .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+                .use('file-loader')
+                .loader('file-loader')
+                .options({
+                    rules: {
+                        emitFile: true, // Don't forget emit images
+                        name: 'img/[name].[hash:8].[ext]'
                     }
-                ]
-            })
+                })
+                .end()
+
+            config
+                .plugin('imagemin-webpack')
+                .use(ImageminWebpackPlugin)
+                .tap(() => {
+                    return [
+                        {
+                            imageminOptions: {
+                                disable: process.env.NODE_ENV !== 'production',
+                                cache: true,
+                                bail: false, // Ignore errors on corrupted images
+                                loader: false,
+                                plugins: [
+                                    imageminGifsicle({
+                                        interlaced: true
+                                    }),
+                                    imageminMozjpeg({
+                                        quality: '75',
+                                        dcScanOpt: 2
+                                    }),
+                                    imageminOptipng({
+                                        optimizationLevel: 5
+                                    })
+                                ]
+                            }
+                        }
+                    ]
+                })
+
+            /* GZIP Compression */
+            config
+                .plugin('gzip')
+                .use(BrotliGzipPlugin)
+                .tap(() => {
+                    return [
+                        {
+                            asset: '[path].gz[query]',
+                            algorithm: 'gzip',
+                            test: /\.(js|css|html|png|jpe?g|gif|webp|tff|woff|woff2|otf)$/,
+                            threshold: 0,
+                            minRatio: 0.8
+                        }
+                    ]
+                })
+
+            /* Brotli Compression */
+            config
+                .plugin('brotli')
+                .use(BrotliGzipPlugin)
+                .tap(() => {
+                    return [
+                        {
+                            asset: '[path].br[query]',
+                            algorithm: 'brotli',
+                            test: /\.(js|css|html|png|jpe?g|gif|webp|tff|woff|woff2|otf)$/,
+                            threshold: 0,
+                            minRatio: 0.8
+                        }
+                    ]
+                })
+        }
 
         /* Inline svg */
         const svgRule = config.module.rule('svg')
         svgRule.uses.clear()
-        svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+        svgRule
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader')
+            .options({
+                limit: 10 * 1024,
+                noquotes: true,
+                svgo: {
+                    plugins: [
+                        {
+                            removeXMLNS: true
+                        }
+                    ]
+                }
+            })
     },
 
     configureWebpack: {
