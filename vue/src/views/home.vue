2e1b8ab34c7ab26bc9168f9e676992b675bd0bfe
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-10-28 00:47:08 +0200

    Firefox fix home icons and placeholder text

--- vue/src/views/home.vue
+++ vue/src/views/home.vue
@@ -2,7 +2,7 @@
     <main>
         <div class="page-wrapper">
             <div class="header">
-                <LangSwitcher/>
+                <LangSwitcher />
             </div>
             <div class="center">
                 <form class="login-form">
@@ -14,7 +14,9 @@
                          style="margin-top: 96px">
                         <div class="input-wrapper"
                              :class="{ 'input-wrapper-error': $v.username.$error }">
-                            <UserIcon class="input-icon" />
+                            <div class="icon-wrapper">
+                                <UserIcon class="input-icon" />
+                            </div>
                             <input type="text"
                                    v-model="username"
                                    @focus="usernameFocused = true"
@@ -24,7 +26,9 @@
                         </div>
                         <div class="input-wrapper"
                              :class="{ 'input-wrapper-error': $v.password.$error }">
-                            <LockIcon class="input-icon" />
+                            <div class="icon-wrapper">
+                                <LockIcon class="input-icon" />
+                            </div>
                             <input type="password"
                                    v-model="password"
                                    @focus="passwordFocused = true"
@@ -64,65 +68,65 @@
 </template>
 
 <script>
-import { required, minLength, maxLength } from "vuelidate/lib/validators";
-import LangSwitcher from "@/components/UI/LangSwitcher.vue";
+import { required, minLength, maxLength } from "vuelidate/lib/validators"
+import LangSwitcher from "@/components/UI/LangSwitcher.vue"
 
 /* SVG */
-import LogoIcon from "@/assets/img/icon/login/logo.svg";
-import UserIcon from "@/assets/img/icon/login/user.svg";
-import LockIcon from "@/assets/img/icon/login/lock.svg";
+import LogoIcon from "@/assets/img/icon/login/logo.svg"
+import UserIcon from "@/assets/img/icon/login/user.svg"
+import LockIcon from "@/assets/img/icon/login/lock.svg"
 
 export default {
-  head() {
-    return {
-      title: this.$t("title.home")
-    };
-  },
-  data() {
-    return {
-      username: "",
-      usernamePH: this.$t("index.form.nameOrEmail"),
-      usernameFocused: false,
-      password: "",
-      passwordPH: this.$t("index.form.password"),
-      passwordFocused: false
-    };
-  },
-  components: {
-    LogoIcon,
-    UserIcon,
-    LockIcon,
-    LangSwitcher
-  },
-  methods: {
-    signIn: async function() {
-      this.$v.$touch();
-      if (!this.$v.$error) {
-        this.call("/v1/auth/login", {
-          username: this.username,
-          password: this.password,
-          stayLoggedIn: false
-        }).then(res => {
-          console.log(res);
-        });
-      } else {
-        console.error("Input error");
-      }
-    }
-  },
-  validations: {
-    username: {
-      required,
-      minLength: minLength(3),
-      maxLength: maxLength(64)
+    head() {
+        return {
+            title: this.$t("title.home")
+        }
     },
-    password: {
-      required,
-      minLength: minLength(4),
-      maxLength: maxLength(72)
+    data() {
+        return {
+            username: "",
+            usernamePH: this.$t("index.form.nameOrEmail"),
+            usernameFocused: false,
+            password: "",
+            passwordPH: this.$t("index.form.password"),
+            passwordFocused: false
+        }
+    },
+    components: {
+        LogoIcon,
+        UserIcon,
+        LockIcon,
+        LangSwitcher
+    },
+    methods: {
+        signIn: async function() {
+            this.$v.$touch()
+            if (!this.$v.$error) {
+                this.call("/v1/auth/login", {
+                    username: this.username,
+                    password: this.password,
+                    stayLoggedIn: false
+                }).then(res => {
+                    console.log(res)
+                })
+            } else {
+                console.error("Input error")
+            }
+        }
+    },
+    validations: {
+        username: {
+            required,
+            minLength: minLength(3),
+            maxLength: maxLength(64)
+        },
+        password: {
+            required,
+            minLength: minLength(4),
+            maxLength: maxLength(72)
+        }
     }
-  }
-};
+}
 </script>
 
 <style src="@/assets/css/pages/index" lang="scss" scoped></style>
