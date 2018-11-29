c21f83cc8cccc2d8fa8d6bfecf204ee78d887057
Daniel Baetz <baetz.daniel@gmail.com>
2018-11-29 14:37:06 +0100

    commented out unused stuff

--- api/lib/error/index.js
+++ api/lib/error/index.js
@@ -8,13 +8,17 @@ function je(message, description) {
     return { error: { message, description } }
 }
 
-export const ERROR_NO_CONTENT = e(204, 'No Content')
-export const ERROR_BAD_REQUEST = e(400, 'Bad Request')
-export const ERROR_NOT_FOUND = e(404, 'Not Found')
-export const ERROR_INTERNAL_SERVER_ERROR = e(500, 'Internal Server Error')
-
-export const SUCCESS = 0
-export const ERROR = 1
+/*************************************************************************
+/* UNUSED
+/************************************************************************* 
+ * export const ERROR_NO_CONTENT = e(204, 'No Content')
+ * export const ERROR_BAD_REQUEST = e(400, 'Bad Request')
+ * export const ERROR_NOT_FOUND = e(404, 'Not Found')
+ * export const ERROR_INTERNAL_SERVER_ERROR = e(500, 'Internal Server Error')
+ * 
+ * export const SUCCESS = 0
+ * export const ERROR = 1
+ *************************************************************************/
 
 export const JERROR_NO_CONTENT = (res, description) => res.status(204).json(je('no content', description))
 export const JERROR_BAD_REQUEST = (res, description) => res.status(400).json(je('bad request', description))
