6acef31e5b83a1e75fb38ff90d969d5fe3453d7c
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-12-05 22:08:10 +0100

    Implemented linting into circleci workflow

--- vue/src/api/lib/util/index.js
+++ vue/src/api/lib/util/index.js
@@ -1,30 +1,30 @@
 // Deprecated
-const SECRETKEY = "mr1CblSuueLdVVmciEYP";
+const SECRETKEY = 'mr1CblSuueLdVVmciEYP'
 
 export function xor_encode(s) {
     if (!s) {
-        return null;
+        return null
     }
-    let enc = "";
-    let str = s.toString();
+    let enc = ''
+    let str = s.toString()
     for (let i = 0; i < str.length; ++i) {
         enc += String.fromCharCode(
             str.charCodeAt(i) ^ SECRETKEY.charCodeAt(i % SECRETKEY.length)
-        );
+        )
     }
-    return new Buffer(enc).toString("base64");
+    return new Buffer(enc).toString('base64')
 }
 
 export function xor_decode(s) {
     if (!s) {
-        return null;
+        return null
     }
-    let enc = "";
-    let str = new Buffer(s.toString(), "base64").toString("ascii");
+    let enc = ''
+    let str = new Buffer(s.toString(), 'base64').toString('ascii')
     for (let i = 0; i < str.length; ++i) {
         enc += String.fromCharCode(
             str.charCodeAt(i) ^ SECRETKEY.charCodeAt(i % SECRETKEY.length)
-        );
+        )
     }
-    return enc;
+    return enc
 }
