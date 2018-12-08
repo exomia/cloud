795f22d785f1c8519191efb45c0235d0748e8370
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-12-08 23:55:14 +0100

    Removing preload and prefetch function

--- vue/server.config.js
+++ vue/server.config.js
@@ -20,24 +20,5 @@ export default {
         './src/api/install'
     ],
     // Watch for changes in these files to automatically reboot server
-    watch: ['src/api/**/*.js'],
-    // Activating prefetch & preload
-    renderer: {
-        // shouldPrefetch: null,
-        shouldPreload: (file, type) => {
-            // type is inferred based on the file extension.
-            // https://fetch.spec.whatwg.org/#concept-request-destination
-            if (type === 'script' || type === 'style') {
-                return true
-            }
-            if (type === 'font') {
-                // only preload woff2 fonts
-                return /\.woff2$/.test(file)
-            }
-            if (type === 'image') {
-                // only preload important images
-                return file === 'login_bg.jpg'
-            }
-        }
-    }
+    watch: ['src/api/**/*.js']
 }
