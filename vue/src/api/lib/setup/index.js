8f4590fe7032e85e9aaee22b72f93fe32d069df1
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-12-08 17:43:46 +0100

    Fixing never used error

--- vue/src/api/lib/setup/index.js
+++ vue/src/api/lib/setup/index.js
@@ -27,7 +27,7 @@ export default () => {
 
         // Check if config exists
         if (
-            fs.stat(path.join(CONFIG_PATH, destFilename), (err, stats) => {
+            fs.stat(path.join(CONFIG_PATH, destFilename), err => {
                 if (err) {
                     console.log(`Creating config file '${destFilename}'`)
                     createConfig(srcFilename, destFilename)
