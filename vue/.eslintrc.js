085cdd8240d84375bf481623dd5419d4368d487c
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-12-08 10:14:04 +0100

    Updated packages

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     },
-    plugins: [cypress],
+    plugins: ['cypress'],
     parserOptions: {
         parser: 'babel-eslint',
         ecmaVersion: 2017,
